98c152859a66b8b1df8aa737157c4426edb3b8a9
Trenton Vanderlinden <vande4tj@cmich.edu>
2017-11-18 15:55:28 -0500

    Test

--- src/Card.java
+++ src/Card.java
@@ -1,35 +1,42 @@
 public class Card {
 
-protected String suit;
-protected int value;
-protected String color;
+	protected String suit;
+	protected int value;
+	protected String color;
+	protected boolean faceDown = true;
 
-public Card(String suit, int value){
-	this.suit = suit;
-	this.value = value;
-	if(suit == "Spades" || suit == "Clubs")
-		color = "Black";
-	else
-		color = "Red";
-}
-
-private void compareTo(Card card1, Card card2){
-	if(card1.color == "Black" && card2.color == "Red" && card2.value == card1.value + 1){
-		//move is allowed
+	public Card(String suit, int value){
+		this.suit = suit;
+		this.value = value;
+		if(suit == "Spades" || suit == "Clubs")
+			color = "Black";
+		else
+			color = "Red";
 	}
-	else if(card1.color == "Red" && card2.color == "Black" && card2.value == card1.value + 1){
-		//Move is allowed
-	}
-	else{
-		System.out.println("Move not allowed. Try another move.");
-	}
-		
-	
-	
-}
 
-public String toString(){
-	return "Suit = " + suit + ", value = " + value + ", color = " + color + "\n";
-}
+	public void faceUp(boolean choice) {
+		if (choice == false) {
+			this.faceDown = false;
+		}
+		else {
+			this.faceDown = true;
+		}
+	}
 
+	private void compareTo(Card card1, Card card2){
+		if(card1.color == "Black" && card2.color == "Red" && card2.value == card1.value + 1){
+			//move is allowed
+		}
+		else if(card1.color == "Red" && card2.color == "Black" && card2.value == card1.value + 1){
+			//Move is allowed
+		}
+		else{
+			System.out.println("Move not allowed. Try another move.");
+		}
+
+	}
+
+	public String toString(){
+		return String.format("Suite: %s, Value: %d, Color: %s, Face-down: %s\n", suit, value, color, faceDown);
+	}
 }
