ece5b29d517ecef5d2ca90507374430fae6c83be
ZachRichard <zrichard26@gmail.com>
2017-11-18 16:15:10 -0500

    Stack For Tableau/Foundation

--- src/Tableau.java
+++ src/Tableau.java
@@ -1,8 +1,17 @@
 import java.util.ArrayList;
 
-public class Tableau {
+public class Tableau implements Stack<Card>{
 	
 	protected ArrayList<Card> Column = new ArrayList<Card>();
+	private Card top = Column.get(Column.size() - 1);
+	
+	
+	
+	public void addTo(Card card) {
+		
+		
+	}
+	
 	
 	
 	public Card topOfTablue() {
